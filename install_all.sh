ca8a9e58ffece50c5e82be35a5a8fbe6e1078aef
shunk-py <auau3700@gmail.com>
2021-04-10 16:37:19 +0900

    add vscode config

---
+++ install_all.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+SRC_DIR="${HOME}/dotfiles/"
+VSCODE_DIR="${HOME}/.config/Code/User"
+
+rm "${VSCODE_DIR}/settings.json"
+ln -s "${SRC_DIR}/settings.json" "${VSCODE_DIR}/settings.json"
+rm "${VSCODE_DIR}/keybindings.json"
+ln -s "${SRC_DIR}/keybindings.json" "${VSCODE_DIR}/keybindings.json"
+
+# install extentions
+cat "${SRC_DIR}/extensions" | while read line
+do
+ code --install-extension $line
+done
+
+code --list-extensions > "${SRC_DIR}/extensions"
