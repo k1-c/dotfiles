8082f8a547a95c85a86231afafd3ad4140ee0c88
shunk-py <shun.kimura@integritis.io>
2022-02-27 14:00:23 +0900

    update

--- dots/.config/fish/config.fish
+++ dots/.config/fish/config.fish
@@ -2,6 +2,7 @@
 if test (identify_os) = "mac"
   set -x PATH /opt/homebrew/bin $PATH
 end
+
 # Debug to disable CORS Policy
 alias dev-browser="chromium-browser --disable-web-security --user-data-dir '/tmp/chrome'"
 # Docker
@@ -14,9 +15,11 @@ alias gco="git branch -a --sort=-authordate | grep -v -e '->' -e '*' | perl -pe 
 alias gr="cd (ghq root)/(ghq list | peco)"
 # vim less
 alias less="/usr/share/vim/vim81/macros/less.sh"
+# Exa
+alias ll="exa -l -g --icons"
+alias lla="ll -a"
 
 ## Golang
-
 set -x GOPATH $HOME/.go
 set -x PATH /usr/local/go/bin $HOME/.go/bin $PATH
 set -x GO111MODULE on
@@ -24,8 +27,12 @@ set -x GO111MODULE on
 ## Python (pyenv)
 set -Ux PYENV_ROOT $HOME/.pyenv
 set -x PATH $PYENV_ROOT/bin $PATH
-status is-login; and pyenv init --path | source
-status is-interactive; and pyenv init - | source
+# status is-login; and pyenv init --path | source
+# status is-interactive; and pyenv init - | source
+pyenv init - | source
+
+## Rust
+set -x PATH $HOME/.cargo/bin $PATH
 
 ## Git Utilities
 
@@ -36,21 +43,26 @@ function remove-merged-branch
     git branch --merged | egrep -v "\*|$PROTECT_BRANCHES" | xargs git branch -d
 end
 
-# pyenv init
+## pyenv init
 # if command -v pyenv 1>/dev/null 2>&1
 #   pyenv init - | source
 # end
 
-# rbenv
+## rbenv
 set -x PATH $HOME/.rbenv/bin $PATH
 status --is-interactive; and source (rbenv init -|psub)
 
-# phpenv
+## phpenv
 set -x PHPENV_ROOT $HOME/.phpenv
 if test -d "$HOME/.phpenv"
   set -x PATH $HOME/.phpenv/bin $PATH
   status --is-interactive; and source (phpenv init -|psub)
 end
 
-# eb cli
+## eb cli
 set -x PATH $HOME/.ebcli-virtual-env/executables $PATH
+
+## Read Local Configurations
+if [ -e $HOME/.config/fish/config.local.fish ]; then
+  source ~/.config/fish/config.local.fish
+end
