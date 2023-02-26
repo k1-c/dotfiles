b0ed3b5ed4482021574e597cc8a4f5c91b60c660
k1-c <shun.kimura@integritis.io>
2023-02-27 05:07:19 +0900

    update

--- env/tools/install.sh
+++ env/tools/install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # tmux: https://github.com/tmux/tmux
-sudo apt install tmux
+sudo apt-get install -y tmux
 
 # exa: https://github.com/ogham/exa
-sudo apt install exa
+sudo apt-get install -y exa
 
 # peco: https://github.com/peco/peco
-sudo apt install peco
+sudo apt-get install -y peco
 
 # ghq: https://github.com/x-motemen/ghq
 go install github.com/x-motemen/ghq@latest
 
 # xdotools and compiz to resize window to custom size using shortcuts
-sudo apt install xdotools compiz
+sudo apt-get install -y xdotools compiz
